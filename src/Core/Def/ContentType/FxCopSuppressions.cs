d47c7cfc45ef32088e9899d2157fc2d3fd7e2c6f
Kirill Osenkov <github@osenkov.com>
2017-10-09 14:35:09 -0700

    Delete some unneeded files.

--- src/Core/Def/ContentType/FxCopSuppressions.cs
+++
@@ -1,8 +0,0 @@
-using System.Diagnostics.CodeAnalysis;
-
-[module: SuppressMessage("Microsoft.Design","CA1018:MarkAttributesWithAttributeUsage", Scope="type", Target="Microsoft.VisualStudio.Utilities.ContentTypeAttribute")]
-[module: SuppressMessage("Microsoft.Design", "CA1018:MarkAttributesWithAttributeUsage", Scope = "type", Target = "Microsoft.VisualStudio.Utilities.FileExtensionAttribute")]
-[module: SuppressMessage("Microsoft.Design", "CA1018:MarkAttributesWithAttributeUsage", Scope = "type", Target = "Microsoft.VisualStudio.Utilities.ExtensionToContentTypeAttribute")]
-[module: SuppressMessage("Microsoft.Design", "CA1019:DefineAccessorsForAttributeArguments", Scope="type", Target="Microsoft.VisualStudio.Utilities.ContentTypeAttribute")]
-[module: SuppressMessage("Microsoft.Design", "CA1040:AvoidEmptyInterfaces", Scope = "type", Target = "Microsoft.VisualStudio.Utilities.IContentTypeDefinition")]
-[module: SuppressMessage("Microsoft.Design", "CA1040:AvoidEmptyInterfaces", Scope = "type", Target = "Microsoft.VisualStudio.Utilities.IFileExtensionToContentTypeDefinition")]
