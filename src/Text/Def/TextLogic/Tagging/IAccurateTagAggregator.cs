1a11e445973c08a5e81e1bdd67f38403c50235e2
Kirill Osenkov <github@osenkov.com>
2017-10-30 13:52:17 -0700

    Move types to match source repo.

--- src/Text/Def/TextLogic/Tagging/IAccurateTagAggregator.cs
+++ src/Text/Def/TextLogic/Tagging/IAccurateTagAggregator.cs
@@ -2,9 +2,6 @@
 //  Copyright (c) Microsoft Corporation. All rights reserved.
 //  Licensed under the MIT License. See License.txt in the project root for license information.
 //
-// This file contain internal APIs that are subject to change without notice.
-// Use at your own risk.
-//
 using System;
 using System.Threading;
 using System.Collections.Generic;
