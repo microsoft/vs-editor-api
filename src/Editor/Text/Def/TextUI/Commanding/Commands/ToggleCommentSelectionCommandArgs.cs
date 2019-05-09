a97adb48dfc32a7395677e46d07544de2d605f96
Aaron Bockover <abock@microsoft.com>
2019-05-09 11:56:20 -0400

    Sync with vs-editor-core@08f0638a

--- src/Editor/Text/Def/TextUI/Commanding/Commands/ToggleCommentSelectionCommandArgs.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Microsoft.VisualStudio.Text.Editor.Commanding.Commands
-{
-    public sealed class ToggleCommentSelectionCommandArgs : EditorCommandArgs
-    {
-        public ToggleCommentSelectionCommandArgs(ITextView textView, ITextBuffer subjectBuffer) : base(textView, subjectBuffer)
-        {
-        }
-    }
-}
