890a74fa28c30f6d7bf0352f2c80b92978e4dc22
Anže Vodovnik <anvod@microsoft.com>
2019-08-06 10:39:09 +0200

    Do Not Merge: Brace Command Consolidation

--- src/Editor/Text/Def/TextUI/Commanding/Commands/GoToMatchingBraceCommandArgs.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Microsoft.VisualStudio.Text.Editor.Commanding.Commands
-{
-    public class GoToMatchingBraceCommandArgs : EditorCommandArgs
-    {
-        public GoToMatchingBraceCommandArgs(ITextView textView, ITextBuffer subjectBuffer) : base(textView, subjectBuffer)
-        {
-        }
-    }
-}
