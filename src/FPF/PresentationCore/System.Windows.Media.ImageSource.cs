eff583c97f3bd789e99d7ec90646782d2cca115d
Aaron Bockover <abock@microsoft.com>
2019-08-20 15:59:12 -0400

    Sync with vs-editor-core@8d85b0cb

--- src/FPF/PresentationCore/System.Windows.Media.ImageSource.cs
+++ src/FPF/PresentationCore/System.Windows.Media.ImageSource.cs
@@ -2,9 +2,7 @@ namespace System.Windows.Media
 {
 	public abstract class ImageSource  : System.Windows.Media.Animation.Animatable
 	{
-
 		public abstract System.Double Height { get; }
 		public abstract System.Double Width { get; }
-
-	}
+    }
 }
